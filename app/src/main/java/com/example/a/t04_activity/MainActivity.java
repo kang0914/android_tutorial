ae7c87663f143b8fc53dcb29ba104654e009df2c
Jaewon <soul0914@naver.com>
2016-05-24 10:01:13 +0900

    empty

--- app/src/main/java/com/example/a/t04_activity/MainActivity.java
+++ app/src/main/java/com/example/a/t04_activity/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.a.t04_activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,5 +34,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, activityTest);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == activityTest){
+            if(resultCode == RESULT_OK){
+                String res = data.getStringExtra("myRes");
+                Toast.makeText(MainActivity.this, "myRes : " + res, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
