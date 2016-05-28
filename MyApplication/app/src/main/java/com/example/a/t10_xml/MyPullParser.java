c74049d354874e972ab88c705a900713a84dabe3
a <jw.kang@cygnusin.com>
2016-05-28 11:29:28 +0900

    1

--- MyApplication/app/src/main/java/com/example/a/t10_xml/MyPullParser.java
+++ MyApplication/app/src/main/java/com/example/a/t10_xml/MyPullParser.java
@@ -3,6 +3,12 @@ package com.example.a.t10_xml;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.net.URL;
+
 /**
  * Created by a on 2016-05-28.
  */
@@ -16,11 +22,56 @@ public class MyPullParser extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... params) {
-        return null;
+        String res = "";
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new URL(params[0]).openStream(), "utf-8");
+
+            int eventType = xpp.getEventType();
+            boolean bRead = false;
+            while(eventType != XmlPullParser.END_DOCUMENT){
+                switch (eventType){
+                    case XmlPullParser.START_TAG:
+                        if(xpp.getName().equals("hour")){
+                            res += "hour : ";
+                            bRead = true;
+                        }else if(xpp.getName().equals("day")){
+                            res += "day : ";
+                            bRead = true;
+                        }
+                        else if(xpp.getName().equals("temp")){
+                            res += "temp : ";
+                            bRead = true;
+                        }
+                        else if(xpp.getName().equals("wfKor")){
+                            res += "wfKor : ";
+                            bRead = true;
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                    case XmlPullParser.TEXT:
+                        if(bRead){
+                            res += xpp.getText();
+                            bRead = false;
+                        }
+                        break;
+                }
+                eventType = xpp.next();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return res;
     }
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+
+        textView.setText(s);
     }
 }
