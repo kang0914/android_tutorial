a5a617aacd07c57ecb4a575f998478b9c3175354
a <jw.kang@cygnusin.com>
2016-05-28 18:24:40 +0900

    +

--- T16_br2/app/src/main/java/com/example/a/t16_br2/MainActivity.java
+++ T16_br2/app/src/main/java/com/example/a/t16_br2/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.a.t16_br2;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if(action.equals(Intent.ACTION_BATTERY_CHANGED)) {
+                int level = intent.getIntExtra("level", 0);
+                Toast.makeText(MainActivity.this, "level : " + level, Toast.LENGTH_SHORT).show();
+            }else if(action.equals(Intent.ACTION_BATTERY_LOW)) {
+                //
+            }else if(action.equals("abcdefg")) {
+                Toast.makeText(MainActivity.this, "custom br ", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        intentFilter.addAction(Intent.ACTION_BATTERY_LOW);
+        intentFilter.addAction("abcdefg");
+
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        unregisterReceiver(receiver);
+    }
+
+    public void onBtnClick(View view){
+        Intent intent = new Intent("abcdefg");
+        sendBroadcast(intent);
     }
 }
