0d556518799d09bcfe52cb9e9236eec20072821b
a <jw.kang@cygnusin.com>
2016-05-28 15:58:49 +0900

    +

---
+++ T14_camera/app/src/main/java/com/example/a/t14_camera/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.a.t14_camera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int PHOTO = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onTakeClicked(View view){
+        String path = Environment.getExternalStorageDirectory() + "/t14.jpg";
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(path)));
+        startActivityForResult(intent, PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == PHOTO){
+            if(resultCode == RESULT_OK){
+                String path = Environment.getExternalStorageDirectory() + "/t14.jpg";
+                Bitmap bitmap;
+
+                bitmap = BitmapFactory.decodeFile(path);
+
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    }
+}
