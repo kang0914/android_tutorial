b7bc9f51febc77a666aa36bacdaa3e8c88bb1f31
a <jw.kang@cygnusin.com>
2016-05-28 17:51:48 +0900

    +

--- T15_br/app/src/main/java/com/example/a/t15_br/MyReceiver.java
+++ T15_br/app/src/main/java/com/example/a/t15_br/MyReceiver.java
@@ -3,6 +3,10 @@ package com.example.a.t15_br;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MyReceiver extends BroadcastReceiver {
     public MyReceiver() {
@@ -10,8 +14,15 @@ public class MyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Object[] pdus = (Object[])intent.getExtras().get("pdus");
+        SmsMessage[] msgs = new SmsMessage[pdus.length];
+        String str = "";
+        for(int i = 0; i < pdus.length; i++){
+            msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+            str += "sms from : " + msgs[i].getOriginatingAddress();
+            str += " : " + msgs[i].getMessageBody() + "\n";
+        }
+
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
     }
 }
