38b9f3929dc6ec01f5e035d3f57fe57a0d3ec695
a <jw.kang@cygnusin.com>
2016-06-04 11:19:03 +0900

    +

--- T19_location/app/src/main/java/com/example/a/t19_location/MainActivity.java
+++ T19_location/app/src/main/java/com/example/a/t19_location/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.a.t19_location;
 
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    LocationManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        String str = "";
+        List<String> providers = manager.getAllProviders();
+        for(int i=0; i<providers.size(); i++) {
+            str += "provider : " + providers.get(i) + "state : " + manager.isProviderEnabled(providers.get(i)) + "\n";
+        }
+        textView.setText(str);
     }
 }
