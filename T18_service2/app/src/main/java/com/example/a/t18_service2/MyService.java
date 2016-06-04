6dce20caa1747e2553e21b6718104f4a9a3b8505
a <jw.kang@cygnusin.com>
2016-06-04 10:46:22 +0900

    +

---
+++ T18_service2/app/src/main/java/com/example/a/t18_service2/MyService.java
@@ -0,0 +1,31 @@
+package com.example.a.t18_service2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.util.Random;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    public class MyBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    private IBinder binder = new MyBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    Random rand = new Random();
+    public int getRandomNumber() {
+        return rand.nextInt(1000);
+    }
+}
