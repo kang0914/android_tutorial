be3e7c8ef8e047bfb9098fe3a3c561ed54f1489a
kang0914 <soul0914g@gmail.com>
2016-12-04 20:27:54 +0900

    Add NavigationDrawer Example

---
+++ NavigationDraw1/app/src/main/java/com/example/jaewon/navigationdraw1/ThirdFragment.java
@@ -0,0 +1,24 @@
+package com.example.jaewon.navigationdraw1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jaewon on 2016-12-03.
+ */
+
+public class ThirdFragment extends Fragment{
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.third_layout, container, false);
+        return myView;
+    }
+}
