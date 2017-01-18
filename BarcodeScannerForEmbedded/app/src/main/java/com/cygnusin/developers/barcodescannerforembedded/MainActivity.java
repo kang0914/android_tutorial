71bc4f1eb501012701baa301b911c3a65b70cb8f
Jaewon <soul0914@naver.com>
2017-01-18 14:37:13 +0900

    Add BarcodeScanner(Embedded)

---
+++ BarcodeScannerForEmbedded/app/src/main/java/com/cygnusin/developers/barcodescannerforembedded/MainActivity.java
@@ -0,0 +1,44 @@
+package com.cygnusin.developers.barcodescannerforembedded;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnScan = (Button) findViewById(R.id.btnScan);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new IntentIntegrator(MainActivity.this).initiateScan(); // `this` is the current Activity
+            }
+        });
+
+        //new IntentIntegrator(this).initiateScan(); // `this` is the current Activity
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
